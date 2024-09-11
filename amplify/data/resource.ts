91c7eaddea80bd3ba40e87c3fa572c1de6bed3d8
vsemi <horace.li@vsemi.io>
2024-09-11 15:19:34 -0400

    added authenticator

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,29 +1,31 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
+import { addUserToGroup } from "./resource"
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
 adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
-const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
-});
 
-export type Schema = ClientSchema<typeof schema>;
+const schema = a.schema({
+  addUserToGroup: a
+    .mutation()
+    .arguments({
+      userId: a.string().required(),
+      groupName: a.string().required(),
+    })
+    .authorization((allow) => [allow.group("ADMINS")])
+    .handler(a.handler.function(addUserToGroup))
+    .returns(a.json())
+})
+
+export type Schema = ClientSchema<typeof schema>
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
+    defaultAuthorizationMode: "iam",
   },
 });
 
